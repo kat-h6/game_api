18c45d832a247f0c62af0babe5cb854920aac0a7
Katharine Hao <hao.katharine@gmail.com>
2021-06-11 22:41:44 +0300

    haven't filtered week and players are repeating but fixed format a bit)

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::StatsController < Api::V1::BaseController
   def show
     @week = "#{Date.commercial(2021, params[:id].to_i, 1)} - #{Date.commercial(2021, params[:id].to_i, 7)}"
     @players = Player.joins(:scores).order(game_score: :desc, time_spent_seconds: :desc)
-    @top_ten = @players.joins(:scores).group(:player_id, :name, :game_score, :time_spent_seconds).pluck(:player_id, :name, 'sum(game_score)', 'sum(time_spent_seconds)')[0..9]
+    @top_ten = @players.joins(:scores).group(:player_id, :name, :game_score, :time_spent_seconds, :timestamp).pluck(:player_id, :name, :timestamp, 'sum(game_score)', 'sum(time_spent_seconds)')[0..9]
   end
 end
--- app/views/api/v1/stats/show.json.jbuilder
+++ app/views/api/v1/stats/show.json.jbuilder
@@ -4,8 +4,9 @@ json.stats do
     json.array! @top_ten do |player|
       json.player_id player[0]
       json.name player[1]
-      json.game_score_sum player[2]
-      json.sum_of_time_spent_in_seconds player[3]
+      json.timestamp player[2].strftime("%m/%d/%Y, %H:%M:%S")
+      json.game_score_sum player[3]
+      json.sum_of_time_spent_in_seconds player[4]
     end
   end
 end
