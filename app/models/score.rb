7588ce5e616866a0ead7e728f8e871275a00554a
Katharine Hao <hao.katharine@gmail.com>
2021-06-12 22:09:31 +0300

    added validations

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,8 +1,8 @@
 class Score < ApplicationRecord
   belongs_to :player
-  validates :game_score, presence: true
+  validates :game_score, presence: true, numericality: { only_integer: true }
   validates :timestamp, presence: true
-  validates :time_spent_seconds, presence: true
+  validates :time_spent_seconds, presence: true, numericality: true
 
   def week(week_number)
     timestamp.strftime('%W') == week_number
